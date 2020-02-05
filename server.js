fe25765991a65b50ee012f606b41ddc6e72931af
Van <vango85@gmail.com>
2020-02-05 15:59:22 +0100

    [xx] Starting point

--- server.js
+++ server.js
@@ -8,18 +8,54 @@ import mongoose from 'mongoose'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Book = mongoose.model('Book', {
+  bookID: {
+    type: String
+  },
+  bookID: {
+    type: Number
+  },
+  title: {
+    type: String
+  },
+  authors: {
+    type: String
+  },
+  average_rating: {
+    type: Number
+  },
+  isbn: {
+    type: String
+  },
+  isbn13: {
+    type: String
+  },
+  language_code: {
+    type: String
+  },
+  num_pages: {
+    type: Number
+  },
+  ratings_count: {
+    type: Number
+  },
+  text_reviews_count: {
+    type: Number
+  }
+});
+
+// booksData.forEach((book) => {
+//   new Book(book).save();
+// });
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -27,10 +63,14 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+Book.find({'title': /Harry/i})
+  .then((results) => {
+    console.log('Found : ' + results);
+  }).catch((err) => {
+    console.log('Error ' + err);
+  });
+
+
 
 // Start the server
 app.listen(port, () => {
