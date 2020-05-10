e39888deae47074d77f1defa3bc45fcbb69c9ca0
Van <vango85@gmail.com>
2020-05-10 22:32:20 +0200

    [xx] Simple book example

--- server.js
+++ server.js
@@ -1,116 +1,81 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from "./data/books.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.Promise = Promise
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/WK18MongoCodealong";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const Book = mongoose.model('Book', {
+const Book = mongoose.model("Book", {
   bookID: {
-    type: Number
+    type: Number,
   },
   title: {
-    type: String
+    type: String,
   },
   authors: {
-    type: String
+    type: String,
   },
   average_rating: {
-    type: Number
+    type: Number,
   },
   isbn: {
     unique: true,
-    type: String
+    type: String,
   },
   isbn13: {
-    type: String
+    type: String,
   },
   language_code: {
-    type: String
+    type: String,
   },
   num_pages: {
-    type: Number
+    type: Number,
   },
   ratings_count: {
-    type: Number
+    type: Number,
   },
   text_reviews_count: {
-    type: Number
-  }
+    type: Number,
+  },
 });
 
-const addBooksToDatabase = () => {
-  booksData.forEach((book) => {
-    new Book(book).save();
-  });
-};
-// addBooksToDatabase();
+if (process.env.RESET_DATABASE) {
+  console.log("Resetting database...");
+  const seedDatabase = async () => {
+    await Book.deleteMany();
+    await booksData.forEach((book) => new Book(book).save());
+  };
+  seedDatabase();
+}
 
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/books', (req, res) => {
-  const queryString = req.query.q;
-  const queryRegex = new RegExp(queryString, "i");
-  Book.find({'title': queryRegex})
-    .sort({'num_pages': -1})
-    .then((results) => {
-      // Succesfull
-      console.log('Found : ' + results);
-      res.json(results);
-    }).catch((err) => {
-      // Error/Failure
-      console.log('Error ' + err);
-      res.json({message: 'Cannot find this book', err: err});
-    });
+app.get("/books", async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
 });
 
-app.get('/books/:isbn', (req, res) => {
+app.get("/books/:isbn", async (req, res) => {
   const isbn = req.params.isbn;
-  Book.findOne({'isbn': isbn})
-    .then((results) => {
-      res.json(results);
-    }).catch((err) => {
-      res.json({message: 'Cannot find this book', err: err});
-    });
-});
-
-app.get('/books/__v/:__v', (req, res) => {
-  const __v = req.params.__v;
-  Book.find({'__v': __v})
-    .then((results) => {
-      res.json(results);
-    }).catch((err) => {
-      res.json({message: 'Cannot find this book', err: err});
-    });
-});
-
-app.get('/books/_id/:_id', (req, res) => {
-  const _id = req.params._id;
-  Book.findOne({'_id': _id})
-    .then((results) => {
-      res.json(results);
-    }).catch((err) => {
-      res.json({message: 'Cannot find this book', err: err});
-    });
+  const book = await Book.findOne({ isbn: isbn });
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ error: `Could not find book with isbn=${isbn}` });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
